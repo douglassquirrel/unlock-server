24f9a30c24280f8114880f6d30acb05f0ddc2340
douglassquirrel <squirrel@blindpages.com>
2010-03-06 22:55:32 +0000

    Move generation of list of sites to controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
+  Site = Struct.new(:name, :link)
+
   def index
+    @sites = [Site.new("BigCo", "/bigco"), Site.new("MeTailer", "/metailer")]
     respond_to do |format|
       format.html # index.html.erb
     end
