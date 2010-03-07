eb02a60098caa1d9e99a41ecc00fbb0a830e28d1
douglassquirrel <squirrel@blindpages.com>
2010-03-07 09:16:31 +0000

    Add unit tests for site

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,12 +1,9 @@
 class Site
-  attr_accessor :name, :short_name
-
-  def initialize(name, short_name)
-    @name       = name
-    @short_name = short_name
-  end
-
   @@sites = []
+
+  def self.clear_all
+    @@sites = []
+  end
 
   def self.register(site)
     @@sites << site
@@ -14,5 +11,16 @@ class Site
 
   def self.all
     return @@sites
+  end
+
+  def self.find_by_short_name(short_name)
+    return @@sites.find { |site| site.short_name == short_name }
+  end
+
+  attr_accessor :name, :short_name
+
+  def initialize(name, short_name)
+    @name       = name
+    @short_name = short_name
   end
 end
