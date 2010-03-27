9abacc6071fec386970aad8393c8dc9c0eb23485
douglassquirrel <squirrel@blindpages.com>
2010-03-27 14:28:48 +0000

    Test various types of phone interaction

--- features/step_definitions/phone_steps.rb
+++ features/step_definitions/phone_steps.rb
@@ -4,7 +4,7 @@ Given /^I have called the service$/ do
   visit '/?format=voicexml'
 end
 
-When /^I select choice ([0-9*#])$/ do |choice|
+When /^(?:then )?I (?:have )?select(?:ed)? choice ([0-9*#])$/ do |choice|
   urls = apply_xpath("/v:vxml/v:form/v:filled/v:if[@cond=\"choice == '#{choice}'\"]/v:submit/@next", body)
   assert (!urls.nil? and urls.size == 1), "Choice #{choice} not available"
   url = "#{urls[0]}?format=voicexml"
