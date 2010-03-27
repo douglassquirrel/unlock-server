dca502e95568a08c091f963b1c949d9fde0ee09f
douglassquirrel <squirrel@blindpages.com>
2010-03-27 00:14:15 +0000

    Check grammar

--- features/step_definitions/phone_content_steps.rb
+++ features/step_definitions/phone_content_steps.rb
@@ -7,8 +7,14 @@ end
 
 Then /^I should hear a menu with this information:$/ do |expected_menu|
   doc = Nokogiri::XML(body)
-  nodes = doc.xpath("/v:vxml/v:form/v:field[@name='choice']/v:prompt/v:prosody/text()", 'v' => 'http://www.w3.org/2001/vxml')
-  actual_menu = [['text']] | nodes.collect { |node| [node.to_s] }
+  menu_nodes = doc.xpath("/v:vxml/v:form/v:field[@name='choice']/v:prompt/v:prosody/text()", 'v' => 'http://www.w3.org/2001/vxml')
+  actual_menu = [['text']] | menu_nodes.collect { |node| [node.to_s] }
   expected_menu.diff! actual_menu
+
+  expected_grammar_items = (1..expected_menu.rows.size).to_a.collect { |i| i.to_s }
+  grammar_item_nodes = doc.xpath("/v:vxml/v:form/v:field[@name='choice']/v:grammar[@xml:lang='en-GB' and @root='top' and @mode='dtmf']/v:rule[@id='top']/v:one-of/v:item/text()", 
+                                 'v' => 'http://www.w3.org/2001/vxml')
+  actual_grammar_items = grammar_item_nodes.collect { |node| node.to_s }
+  assert_equal expected_grammar_items, actual_grammar_items, "Grammar should list expected DTMF tones"
 end
 
--- features/step_definitions/validation_steps.rb
+++ features/step_definitions/validation_steps.rb
@@ -8,7 +8,7 @@ Then /^the service should provide valid VoiceXML with correct settings$/ do
   validate(body, %r|<!DOCTYPE vxml PUBLIC "-//W3C//DTD VOICEXML 2.1//EN" "http://www.w3.org/TR/voicexml21/vxml.dtd"|, 'vxml.xsd')
   check_present(body, "/v:vxml/v:property[@name='inputmodes' and @value='dtmf']", "Should limit input to DTMF tones")
   check_present(body, "/v:vxml/v:form/v:block/v:prompt[1]/v:break[@time='1000ms']", "Should pause before speaking")
-  check_not_present(body, "/v:vxml/v:form/v:block/v:prompt[v:prosody and not(v:break[@time='500ms'])]", "Should pause after each prompt")
+  check_not_present(body, "//v:prompt[v:prosody and not(v:break[@time='500ms'])]", "Should pause after each prompt")
 end
 
 
